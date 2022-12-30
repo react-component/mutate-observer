e88d9198ea96a66830103a05983561668d12bead
栗嘉男 <574980606@qq.com>
2022-12-30 11:31:00 +0800

    add test case

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render } from '@testing-library/react';
 import MutateObserver from '../src';
 
 describe('MutateObserver', () => {
-  it('support onMutate', () => {
+  it('MutateObserver should support onMutate', () => {
     const fn = jest.fn();
     const Demo: React.FC = () => {
       const [flag, setFlag] = React.useState<boolean>(true);
@@ -27,34 +27,34 @@ describe('MutateObserver', () => {
     }
     unmount();
   });
-});
 
-it('findDOMNode should not error in React.StrictMode', () => {
-  const fn = jest.fn();
-  const buttonRef = React.createRef<HTMLButtonElement>();
-  const warnSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-  const Demo = React.forwardRef<
-    HTMLButtonElement,
-    React.HTMLAttributes<HTMLButtonElement>
-  >((props, ref) => {
-    const [flag, setFlag] = React.useState<boolean>(true);
-    return (
-      <React.StrictMode>
-        <MutateObserver onMutate={fn}>
-          <button
-            {...props}
-            ref={ref}
-            className={flag ? 'aaa' : 'bbb'}
-            onClick={() => setFlag(!flag)}
-          >
-            click
-          </button>
-        </MutateObserver>
-      </React.StrictMode>
-    );
+  it('findDOMNode should not error in React.StrictMode', () => {
+    const fn = jest.fn();
+    const buttonRef = React.createRef<HTMLButtonElement>();
+    const warnSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const Demo = React.forwardRef<
+      HTMLButtonElement,
+      React.HTMLAttributes<HTMLButtonElement>
+    >((props, ref) => {
+      const [flag, setFlag] = React.useState<boolean>(true);
+      return (
+        <React.StrictMode>
+          <MutateObserver onMutate={fn}>
+            <button
+              {...props}
+              ref={ref}
+              className={flag ? 'aaa' : 'bbb'}
+              onClick={() => setFlag(!flag)}
+            >
+              click
+            </button>
+          </MutateObserver>
+        </React.StrictMode>
+      );
+    });
+    const { container } = render(<Demo ref={buttonRef} />);
+    fireEvent.click(container.querySelector('button')!);
+    expect(warnSpy).not.toHaveBeenCalled();
+    warnSpy.mockRestore();
   });
-  const { container } = render(<Demo ref={buttonRef} />);
-  fireEvent.click(container.querySelector('button')!);
-  expect(warnSpy).not.toHaveBeenCalled();
-  warnSpy.mockRestore();
 });
